ede14c102b410856c7c17672d9a60c3448e512d2
kihwan <kihwan0624@hotmail.com>
2021-03-26 07:21:17 +0900

    commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("ddd")
+import pymysql
+connection = pymysql.connect(host='localhost', port=3306, db='stock', user='root', passwd='3636',autocommit=True)
+
+cursor = connection.cursor()
+cursor.execute("SELECT VERSION();")
+result = cursor.fetchone()
+
+print("MariaDB version :{}".format(result))
+
+connection.close()
