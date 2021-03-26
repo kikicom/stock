0ae202a2f80562d96796c28e51e6fbbb1e34c39d
kikicom <kihwan0624@hotmail.com>
2021-03-26 14:19:44 +0900

    업데이트

--- DBUpdateer.py
+++ DBUpdateer.py
@@ -1,8 +1,10 @@
-import pymysql
 import pandas as pd
 from bs4 import BeautifulSoup
+import urllib, calendar, time, json
 from datetime import datetime
 from urllib.request import urlopen
+from threading import Timer
+import pymysql
 
 class DBUpdater:
     def __init__(self):
@@ -18,6 +20,7 @@ class DBUpdater:
                     UPD_DTM DATE,
                     PRIMARY KEY (CODE)
                 )
+                
             """
             curs.execute(sql)
             sql = """
@@ -106,15 +109,53 @@ class DBUpdater:
         except Exception as e:
             print('Exception occured : ', str(e))
             return None
+        return df
 
     def replace_into_db(self, df, num, code, company):
         """네이버 금융에서 읽어온 주식 시세를 DB에 REPLACE"""
+        with self.conn.cursor() as curs:
+            for r in df.itertuples():
+                sql = "REPLACE INTO tb_daily_price VALUES ('{}','{}',{},{},{},{},{},{}".format(code, r.date, r.open, r.high, r.low, r.close, r.diff, r.volume)
+                curs.execute(sql)
+            self.conn.commit()
+            print('[{}] {:04d} {} ({}) : {} rows > REPLACE INTO daily_price [OK]'.format(datetime.now().strftime('%Y-%m-%d %H:%M'), num+1, company, code, len(df)))
 
     def update_daily_price(self, pages_to_fetch):
         """KRX 상장법인의 주식 시세를 네이버로부터 읽어서 DB에 업데이트"""
+        for idx, code in enumerate(self.codes):
+            df = self.read_naver(code, self.codes[code], pages_to_fetch)
+            if df is None:
+                continue
+            self.replace_into_db(df, idx, code, self.codes[code])
 
     def execute_daily(self):
         """실행 즉시 }및 매일 오후 다섯시에 daily_price 테이블 업데이트"""
+        self.update_comp_info()
+        try:
+            with open('config.json','r') as in_file:
+                config = json.load(in_file)
+                pages_to_fetch = config['pages_to_fetch']
+        except FileNotFoundError:
+            with open('config.json', 'w') as out_file:
+                pages_to_fetch = 100
+                config ={'pages_to_fetch': 1}
+                json.dump(config, out_file)
+        self.update_daily_price(pages_to_fetch)
+
+        tmnow = datetime.now()
+        lastday = calendar.monthrange(tmnow.year, tmnow.month)[1]
+        if tmnow.month == 12 and tmnow.day == lastday:
+            tmnext = tmnow.replace(year=tmnow.year+1, month=1, ay=1, hour=17, minute=0, second=0)
+        elif tmnow.day == lastday:
+            tmnext = tmnow.replace(month=tmnow.month+1, day=1, hour=17, minute=0, second=0)
+        else:
+            tmnext = tmnow.replace(day=tmnow.day+1, hour=17, minute=0, second=0)
+        tmdiff = tmnext - tmnow
+        secs = tmdiff.seconds
+
+        t = Timer(secs, self.execute_daily)
+        print("Waiting for next update ({})... ".format(tmnext.strftime('%Y-%m-%d %H:%M')))
+        t.start()
 
 if __name__ == '__main__':
     dbu = DBUpdater()
